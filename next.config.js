b60f05ac53ab040e35336c770911356e60d81531
Flexasaurusrex <108902627+Flexasaurusrex@users.noreply.github.com>
2025-09-24 10:53:48 -0700

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,36 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    serverComponentsExternalPackages: ['@prisma/client'],
+  },
+  images: {
+    domains: [
+      'localhost',
+      'replicate.delivery',
+      'pbxt.replicate.delivery',
+    ],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**.supabase.co',
+      },
+      {
+        protocol: 'https',
+        hostname: '**.amazonaws.com',
+      },
+    ],
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+        path: false,
+        os: false,
+      };
+    }
+    return config;
+  },
+};
+
+module.exports = nextConfig;
