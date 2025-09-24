4c2ee0f02a73a6e7e2c87c4738020ffa4f158553
Flexasaurusrex <108902627+Flexasaurusrex@users.noreply.github.com>
2025-09-24 11:30:57 -0700

    Create Root.tsx

---
+++ remotion/Root.tsx
@@ -0,0 +1,24 @@
+import { Composition } from 'remotion';
+import { VideoComposition } from './VideoComposition';
+
+export const RemotionRoot: React.FC = () => {
+  return (
+    <Composition
+      id="ArtNarratorVideo"
+      component={VideoComposition}
+      durationInFrames={1800} // 60 seconds at 30fps
+      fps={30}
+      width={1080}
+      height={1920}
+      defaultProps={{
+        scenes: [],
+        textStyle: null,
+        musicTrack: null,
+        projectSettings: {
+          fps: 30,
+          bgColor: '#000000',
+        },
+      }}
+    />
+  );
+};
