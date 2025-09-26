93761f6c6b58a54e0844ee805316c32bdea781c0
Flexasaurusrex <108902627+Flexasaurusrex@users.noreply.github.com>
2025-09-25 17:45:22 -0700

    Update Root.tsx

--- remotion/Root.tsx
+++ remotion/Root.tsx
@@ -1,23 +1,18 @@
 import { Composition } from 'remotion';
-import { VideoComposition } from './VideoComposition';
+import { VideoEssayComposition } from './VideoEssayComposition';
 
 export const RemotionRoot: React.FC = () => {
   return (
     <Composition
       id="ArtNarratorVideo"
-      component={VideoComposition as any}
-      durationInFrames={1800} // 60 seconds at 30fps
+      component={VideoEssayComposition as any}
+      durationInFrames={1800} // Will be calculated dynamically based on scenes
       fps={30}
       width={1080}
       height={1920}
       defaultProps={{
         scenes: [],
-        textStyle: null,
-        musicTrack: null,
-        projectSettings: {
-          fps: 30,
-          bgColor: '#000000',
-        },
+        backgroundMusic: null,
       }}
     />
   );
