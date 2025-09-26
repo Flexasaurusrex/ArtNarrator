84d8149f008a1f3204c198ce09fc9551c74d46a2
Flexasaurusrex <108902627+Flexasaurusrex@users.noreply.github.com>
2025-09-26 05:18:19 -0700

    Update Root.tsx

--- remotion/Root.tsx
+++ remotion/Root.tsx
@@ -3,17 +3,19 @@ import { VideoEssayComposition } from './VideoEssayComposition';
 
 export const RemotionRoot: React.FC = () => {
   return (
-    <Composition
-      id="ArtNarratorVideo"
-      component={VideoEssayComposition as any}
-      durationInFrames={1800} // Will be calculated dynamically based on scenes
-      fps={30}
-      width={1080}
-      height={1920}
-      defaultProps={{
-        scenes: [],
-        backgroundMusic: null,
-      }}
-    />
+    <>
+      <Composition
+        id="ArtNarratorVideo"
+        component={VideoEssayComposition}
+        durationInFrames={1800} // Will be overridden by API
+        fps={30}
+        width={1080}
+        height={1920}
+        defaultProps={{
+          scenes: [],
+          backgroundMusic: null,
+        }}
+      />
+    </>
   );
 };
