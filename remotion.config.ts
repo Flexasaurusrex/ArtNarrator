af6e23ba1823c06abcbd787282f5b19606c529d8
Flexasaurusrex <108902627+Flexasaurusrex@users.noreply.github.com>
2025-09-26 05:20:30 -0700

    Update remotion.config.ts

--- remotion.config.ts
+++ remotion.config.ts
@@ -3,4 +3,6 @@ import { Config } from '@remotion/cli/config';
 Config.setVideoImageFormat('jpeg');
 Config.setOverwriteOutput(true);
 Config.setPixelFormat('yuv420p');
-Config.setCodec('h264');
+Config.setConcurrency(1);
+Config.setQuality(80);
+Config.setTimeout(240000); // 4 minutes
