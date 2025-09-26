a64c7ebdb00ac3f5215d1ba19de9c0b7adccc1cb
Flexasaurusrex <108902627+Flexasaurusrex@users.noreply.github.com>
2025-09-26 05:32:29 -0700

    Update remotion.config.ts

--- remotion.config.ts
+++ remotion.config.ts
@@ -1,3 +1,7 @@
+// Remotion configuration disabled while packages are not installed
+// Uncomment and install @remotion packages to enable
+
+/*
 import { Config } from '@remotion/cli/config';
 
 Config.setVideoImageFormat('jpeg');
@@ -6,3 +10,6 @@ Config.setPixelFormat('yuv420p');
 Config.setConcurrency(1);
 Config.setQuality(80);
 Config.setTimeout(240000); // 4 minutes
+*/
+
+export {};
